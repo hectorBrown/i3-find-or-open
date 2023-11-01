649760153338d8504cd593b6613f7d202c59171c
Hector Brown <hectorjbrown@protonmail.com>
2023-11-01 22:51:34 +0000

    move to argparse for argument parsing

--- i3_find_or_open/main.py
+++ i3_find_or_open/main.py
@@ -1,9 +1,9 @@
 """A module that helps you bind keys to find and display, or open a window in i3wm."""
 #!/bin/python
+import argparse
 import json
 import re
 import subprocess as sp
-import sys
 
 import flatdict
 
@@ -42,13 +42,28 @@ def find_window(tree: dict, title: str) -> int | None:
 
 def main():
     """Main function for binary."""
-    title = sys.argv[1]
-    command = sys.argv[2]
+    # parse arguments
+    parser = argparse.ArgumentParser(
+        prog="i3-find-or-open",
+        description="A command-line utility that helps you bind keys to find and display a window, or open it if there is no instance running in i3wm.",  # noqa: E501
+    )
+    parser.add_argument(
+        "title",
+        type=str,
+        help="A regex that will match the window title that you are trying to find.",
+    )
+    parser.add_argument(
+        "command",
+        type=str,
+        help="The command that will be run if the window is not open.",
+    )
+    args = parser.parse_args()
+
     tree = json.loads(sp.run(["i3-msg", "-t", "get_tree"], capture_output=True).stdout)
     if len(
         list(
             filter(
-                re.compile(title).match,
+                re.compile(args.title).match,
                 [
                     x["nodes"][0]["name"]
                     for x in tree["nodes"][0]["nodes"][0]["nodes"][0]["floating_nodes"]
@@ -57,16 +72,16 @@ def main():
         )
     ):
         sp.run(
-            f"i3-msg '[title=\"{title}\"] scratchpad show'",
+            f"i3-msg '[title=\"{args.title}\"] scratchpad show'",
             shell=True,
         )
     else:
         output = 1
         ws = None
         while ws is None and output < len(tree["nodes"]):
-            ws = find_window(tree["nodes"][output]["nodes"][1]["nodes"], title)
+            ws = find_window(tree["nodes"][output]["nodes"][1]["nodes"], args.title)
             output += 1
         if ws is not None:
             sp.run(f"i3-msg 'workspace {ws}' &> /dev/null", shell=True)
         else:
-            sp.run(command, shell=True)
+            sp.run(args.command, shell=True)
